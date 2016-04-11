b91e2f0dcfbc5175a15e677277a7ba8716d83d1c
Summer Wollin <summer.wollin@gmail.com>
2016-04-11 10:06:30 -0600

    basic passport set-up

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var LinkedInStrategy = require('passport-github2').Strategy;
+var passport = require('passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
