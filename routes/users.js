aa0b89feedfebd05305e1456073b70450140ceee
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-04-11 10:50:47 -0600

    Boiler Plate GET Routes Set

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users!' });
 });
+
+router.get('/:id', function(req, res, next){
+  res.render('userDetails', {title: 'User Details!', id: req.params.id})
+})
+
+router.get('/:id/edit', function(req, res, next) {
+  res.render('userEdit', {title: 'User Edit!', id: req.params.id})
+})
+
 
 module.exports = router;
