dde4e139c877a3bff3de948211f8bf94aaf647e3
Summer Wollin <summer.wollin@gmail.com>
2016-04-11 11:19:57 -0600

    add app id and secret for github, define oauth callback url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var LinkedInStrategy = require('passport-github2').Strategy;
+var GitHubStrategy = require('passport-github2').Strategy;
 var passport = require('passport');
 
 var routes = require('./routes/index');
@@ -26,6 +26,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/auth/github', passport.authenticate('github'), function(req, res){
+  // The request will be redirected to LinkedIn for authentication, so this
+  // function will not be called.
+});
+
+app.get('/auth/github/callback', passport.authenticate('github', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.HOST + "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.use('/', routes);
 app.use('/users', users);
