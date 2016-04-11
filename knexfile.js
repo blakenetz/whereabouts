0490fac7a7d50c0468557eb48af26c18993359a2
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-11 10:07:46 -0600

    added npm pg. added /wireframes. added data table model. added first migration. update readme to include project spreadsheet

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,11 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 'postgres://localhost/maps',
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 
