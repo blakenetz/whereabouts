23cef9c3d05d29e3759d2888a2e13593150e2e72
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-11 11:06:15 -0600

    table skeleton

--- migrations/20160411095848_create-tables.js
+++ migrations/20160411095848_create-tables.js
@@ -1,10 +1,28 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function ( table ){
-    tables.increments();
+  return knex.schema
+  .createTable('users', function ( table ){
+    table.increments();
     table.string('username');
-    table.string('password');
+    table.string('avatar');
     table.boolean('admin');
+  })
+  .createTable('posts', function ( table ){
+    table.increments();
+    table.string('title');
+    table.string('description');
+    table.integer('rating');
+    table.integer('user_id');
+    table.integer('location');
+    table.string('imgLink');
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+  })
+  .createTable('comments', function ( table ){
+    table.increments();
+    table.integer('user_id');
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+    table.string('comment');
+    table.integer('post_id')
   })
 };
 
