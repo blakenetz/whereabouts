38602d7806270a3fe8e7d447a64c5be377beee1b
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-11 11:20:36 -0600

    completed first migration

--- migrations/20160411095848_create-tables.js
+++ migrations/20160411095848_create-tables.js
@@ -10,19 +10,19 @@ exports.up = function(knex, Promise) {
   .createTable('posts', function ( table ){
     table.increments();
     table.string('title');
-    table.string('description');
+    table.string('description', 2500);
     table.integer('rating');
-    table.integer('user_id');
+    table.integer('user_id').unsigned().references('users.id');
     table.integer('location');
-    table.string('imgLink');
+    table.string('imgLink', 500);
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
   })
   .createTable('comments', function ( table ){
     table.increments();
-    table.integer('user_id');
+    table.integer('user_id').unsigned().references('users.id');
     table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
-    table.string('comment');
-    table.integer('post_id')
+    table.string('comment', 1000);
+    table.integer('post_id').unsigned().references('posts.id');
   })
 };
 
