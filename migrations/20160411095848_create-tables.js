c8ecbd67f58aeb21c760f802dd4d32fe6b618082
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-11 13:21:28 -0600

    updated users table to include username and password

--- migrations/20160411095848_create-tables.js
+++ migrations/20160411095848_create-tables.js
@@ -3,7 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema
   .createTable('users', function ( table ){
     table.increments();
+    table.string('email');
     table.string('username');
+    table.string('password');
     table.string('avatar');
     table.boolean('admin');
   })
