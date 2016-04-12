2314866ca926688245157310d970bba201402b95
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-04-12 10:15:16 -0600

    sockets are working!

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
 var unirest = require('unirest');
-var GoogleLocations = require('google-locations');
-var locations = new GoogleLocations('AIzaSyCY6IF5KZuqMBt61jyVNQshduxTFUArNZY');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
