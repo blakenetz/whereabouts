1be513fe200379b585011fc638f73a9c4cb177ad
Summer Wollin <summer.wollin@gmail.com>
2016-04-12 10:45:54 -0600

    comment out issue blocks

--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,19 @@ const isAdmin = require("../users.js").isAdmin;
 const authorizedUser = require("../users.js").authorizedUser;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    let user_id = req.session.id;
-    // let admin = req.signedCookies.admin;
-    if (isAdmin === true) {
-        knex('users').then(function(users){
-            res.status(200).render('users', {
-              users: users
-            })
-        })
-    } else {
-        res.redirect('/');
-    }
-});
+// router.get('/', function(req, res, next) {
+//     let user_id = req.session.id;
+//     // let admin = req.signedCookies.admin;
+//     if (isAdmin === true) {
+//         knex('users').then(function(users){
+//             res.status(200).render('users', {
+//               users: users
+//             })
+//         })
+//     } else {
+//         res.redirect('/');
+//     }
+// });
 
 router.get('/:id', function(req, res, next){
   res.render('userDetails', {title: 'User Details!', id: req.params.id})
