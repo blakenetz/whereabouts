2c3d76c2ea930055b77a2d847f442351c8d2b918
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-11 18:28:29 -0600

    added navbar

--- seeds/a.js
+++ seeds/a.js
@@ -1,4 +1,4 @@
-exports.seed = function(knex, Promise) {
+nodexports.seed = function(knex, Promise) {
 
     return knex('posts').del().then(function(){
       return knex('users').del().then(function(){
