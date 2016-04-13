d6166a7bfea1b90dbd1aa76c01e435fcd38127bc
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-12 21:13:24 -0600

    stylized postdetails page

--- routes/posts.js
+++ routes/posts.js
@@ -29,7 +29,7 @@ router.get('/:id', function(req, res, next) {
                                   id: req.params.id,
                                   post: post,
                                   comments: comments,
-                                }):
+                                })
     })
   })
 });
