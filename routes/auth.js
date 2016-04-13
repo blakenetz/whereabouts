ae31d1d7ef5be02245c340b257111f7679723c25
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-13 13:37:27 -0600

    added form validation w/ post edit, post create, signup

--- routes/auth.js
+++ routes/auth.js
@@ -5,31 +5,53 @@ var bcrypt = require('bcrypt');
 
 
 router.post('/signup', function(req, res, next){
-  knex('users').where({
-    email: req.body.email
-  }).first().then(function(user){
-    if(!user){
-     var salt = Math.floor(Math.random() * 0xFFFF);
-     var saltedPassword = req.body.password + salt;
+  if (req.body.username.length < 1 || req.body.username !== /\s/g) {
+    res.render('signup', {
+      erra : "Please enter a valid username",
+      body : req.body
+    })
+  } if (req.body.email.length < 1 || req.body.email !== /[@.]/g) {
+    res.render('signup', {
+      errb : "Please enter a valid email address",
+      body : req.body
+    })
+  } if (req.body.password.length < 1) {
+    res.render('signup', {
+      errc : "Please add a password",
+      body : req.body
+    })
+  } if (req.body.avatar.length < 1) {
+    res.render('signup', {
+      errd : "Please add a URL to your avatar",
+      body : req.body
+    })
+  } else {
+    knex('users').where({
+      email: req.body.email
+    }).first().then(function(user){
+      if(!user){
+        var salt = Math.floor(Math.random() * 0xFFFF);
+        var saltedPassword = req.body.password + salt;
 
-     var hash = bcrypt.hashSync(saltedPassword, 10);
-     knex('users').insert({
-       email: req.body.email,
-       username: req.body.username,
-       password: hash,
-       salt: salt,
-       avatar: req.body.avatar,
-       auth_strategy: 'local'
-     }).then(function(){
-       res.redirect('/');
-     }).catch(function(err){
-       console.log(err);
-       res.redirect('/login')
-     })
-   } else {
-     res.redirect('/login')
-    }
-  })
+        var hash = bcrypt.hashSync(saltedPassword, 10);
+        knex('users').insert({
+          email: req.body.email,
+          username: req.body.username,
+          password: hash,
+          salt: salt,
+          avatar: req.body.avatar,
+          auth_strategy: 'local'
+        }).then(function(){
+          res.redirect('/');
+        }).catch(function(err){
+          console.log(err);
+          res.redirect('/login')
+        })
+      } else {
+        res.redirect('/login')
+      }
+    })
+  }
 })
 
 
