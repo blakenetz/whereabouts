7177e6590aae732ca0db04fa8fac1fa813fefe56
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-14 17:25:22 -0600

    fixed signup validation typo

--- routes/auth.js
+++ routes/auth.js
@@ -6,22 +6,22 @@ var bcrypt = require('bcrypt');
 
 router.post('/signup', function(req, res, next){
   if (req.body.username.length < 1 || req.body.username == /\s/g) {
-    res.render('signup', {
+    res.render('signUp', {
       erra : "Please enter a valid username",
       body : req.body
     })
   } else if ( /^[^@]+@[^@]+\.[^@]+$/.test(req.body.email) === false ) {
-    res.render('signup', {
+    res.render('signUp', {
       errb : "Please enter a valid email address",
       body : req.body
     })
   } else if (req.body.password.length < 4) {
-    res.render('signup', {
+    res.render('signUp', {
       errc : "Passwords must contain 4 or more characters",
       body : req.body
     })
   } else if (req.body.avatar.length < 1) {
-    res.render('signup', {
+    res.render('signUp', {
       errd : "Please add a URL to your avatar",
       body : req.body
     })
@@ -44,7 +44,7 @@ router.post('/signup', function(req, res, next){
         }).then(function(){
           res.render('login', {username: req.body.username});
         }).catch(function(err){
-          res.render('signup', {
+          res.render('signUp', {
             erre : "Username already exists",
             body: req.body
           })
