702f9b5607e468a04c4857577d8ac0bc016209c5
Nick MacIntyre <nickmac@Nick-MacBook-Air.local>
2016-04-14 18:10:25 -0600

    fixed router errors

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
 
+
 router.get('/', function (req, res, next){
   console.log('yo face');
   knex('users')
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,22 @@
-'use strict';
-
 var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV]);
-const isAdmin = require("../userAuth.js").isAdmin;
-const authorizedUser = require("../userAuth.js").authorizedUser;
 
 
-router.get('/:id', function(req, res, next){
+function checkNum (req, res, next) {
+  if (+req.params.id > 0) {
+    next();
+  } else {
+    res.redirect('/')
+  }
+}
+
+router.get('/:id', checkNum, function(req, res, next){
   knex('users').where({user_id: req.params.id})
   .then( function (user) {
+    if(user[0] === undefined){
+      res.redirect('/');
+    };
     res.render('userDetails', {user: user})
   })
 })
